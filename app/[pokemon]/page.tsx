40f3a53ebdac1c88f1e7a38fe5e3b9b431e768bc
Gian Denorte <67414853+Manggigi@users.noreply.github.com>
2023-10-14 12:40:12 +0800

    feat: show list of pokemon details using server component

---
+++ app/[pokemon]/page.tsx
@@ -0,0 +1,45 @@
+import PokeAPI from "pokeapi-typescript";
+import Image from "next/image";
+
+async function getPokemon(nameOrId: string) {
+  const result = await PokeAPI.Pokemon.resolve(nameOrId);
+  return {
+    name: result.name,
+    sprites: result.sprites,
+    id: result.id,
+  };
+}
+
+type Props = {
+  params: { pokemon: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+const Pokemon = async (props: Props) => {
+  const { params, searchParams } = props;
+  const pokemon = await getPokemon(params.pokemon);
+
+  return (
+    <div>
+      <a href="/" type="button">
+        back
+      </a>
+      <div style={{ marginTop: "1em" }}>
+        <p>params: {JSON.stringify(params)}</p>
+        <p>searchParams: {JSON.stringify(searchParams)}</p>
+        {/* <p>data: {JSON.stringify(pokemon, null, 2)}</p> */}
+      </div>
+
+      <p style={{ marginTop: "2em" }}>{pokemon.name}</p>
+      <p>{pokemon.id}</p>
+      <Image
+        src={pokemon.sprites.front_default || ""}
+        alt={pokemon.name}
+        width={100}
+        height={100}
+      />
+    </div>
+  );
+};
+
+export default Pokemon;
